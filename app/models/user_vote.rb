f7e1d3e7a80b957eb16ac16a1ab82fa11d228c47
Ken Urata <senseiurata@gmail.com>
2014-05-22 14:26:18 -0700

    implement design on front page / deal show page

--- app/models/user_vote.rb
+++ app/models/user_vote.rb
@@ -2,7 +2,5 @@ class UserVote < ActiveRecord::Base
   validates :user, :votable, presence: true
 
   belongs_to :user, inverse_of: :user_votes
-#  belongs_to :deal, inverse_of: :user_votes
-
   belongs_to :votable, polymorphic: true
 end
