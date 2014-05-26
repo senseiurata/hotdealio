163a5f1bcbb874f1073b9dd2773a5c3b60f1de28
Ken Urata <senseiurata@gmail.com>
2014-05-26 10:25:26 -0700

    set config settings in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+
+  config.server_static_assets = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
