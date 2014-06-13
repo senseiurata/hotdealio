63b954841d29d0f5c5470ee4b2491464d9c17d3c
Ken Urata <senseiurata@gmail.com>
2014-06-13 03:40:40 -0700

    delete unnecessary files, fix css syntax

--- config/routes.rb
+++ config/routes.rb
@@ -2,27 +2,11 @@ Rails.application.routes.draw do
   resources :users, only: [:create]
   resource :session, only: [:create, :destroy]
 
-  # resources :categories, only: [:show, :index]
-  # resources :deals, only: [:new, :create, :edit, :update, :destroy, :show] do
-  #   resources :comments, only: [:create, :new, :show] do
-  #     member do
-  #       post "upvote"
-  #       post "downvote"
-  #     end
-  #   end
-  #   member do
-  #     post "upvote"
-  #     post "downvote"
-  #   end
-  # end
-
-
   namespace :api, defaults: { format: :json } do
     get "deals/recent", to: "deals#recent"
     get "deals/today", to: "deals#today"
     get "deals/past7", to: "deals#past7"
-    # resources :users, only: [:create]
-    # resource :session, only: [:create, :destroy]
+
     resources :categories, only: [:show, :index, :create] do
       get "today", to: "categories#today"
       get "past7", to: "categories#past7"
